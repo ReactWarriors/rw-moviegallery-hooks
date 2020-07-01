d6524e5a7f2f017e7d9502fd693236a0f9d52369
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2020-07-01 18:12:24 +0300

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,123 @@
+import React from "react";
+import MovieItem from "./MovieItem";
+import { API_URL, API_KEY_3 } from "../utils/api";
+import MovieTabs from "./MovieTabs";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      moviesWillWatch: [],
+      sort_by: "revenue.desc",
+    };
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.sort_by !== this.state.sort_by) {
+      this.getMovies();
+    }
+  }
+
+  getMovies = () => {
+    fetch(
+      `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${this.state.sort_by}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          movies: data.results,
+        });
+      });
+  };
+
+  deleteMovie = (movie) => {
+    const updateMovies = this.state.movies.filter(
+      (item) => item.id !== movie.id
+    );
+
+    this.setState({
+      movies: updateMovies,
+    });
+  };
+
+  addMovieToWillWatch = (movie) => {
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    updateMoviesWillWatch.push(movie);
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch,
+    });
+  };
+
+  deleteMovieFromWillWatch = (movie) => {
+    const updateMoviesWillWatch = this.state.moviesWillWatch.filter(
+      (item) => item.id !== movie.id
+    );
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch,
+    });
+  };
+
+  updateSortBy = (value) => {
+    this.setState({
+      sort_by: value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-9">
+            <div className="row mb-4">
+              <div className="col-12">
+                <MovieTabs
+                  sort_by={this.state.sort_by}
+                  updateSortBy={this.updateSortBy}
+                />
+              </div>
+            </div>
+            <div className="row">
+              {this.state.movies.map((movie) => {
+                return (
+                  <div className="col-6 mb-4" key={movie.id}>
+                    <MovieItem
+                      data={movie}
+                      deleteMovie={this.deleteMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                      deleteMovieFromWillWatch={this.deleteMovieFromWillWatch}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-3">
+            <h4>Will Watch: {this.state.moviesWillWatch.length} movies</h4>
+            <ul className="list-group">
+              {this.state.moviesWillWatch.map((movie) => (
+                <li key={movie.id} className="list-group-item">
+                  <div className="d-flex justify-content-between">
+                    <p>{movie.title}</p>
+                    <p>{movie.vote_average}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MovieTabs.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+class MovieTabs extends React.Component {
+  render() {
+    const { sort_by, updateSortBy } = this.props;
+
+    const handleClick = (value) => {
+      return (event) => {
+        updateSortBy(value);
+      };
+    };
+
+    const getClassLink = (value) => {
+      return `nav-link ${sort_by === value ? "active" : ""}`;
+    };
+
+    return (
+      <ul className="tabs nav nav-pills">
+        <li className="nav-item">
+          <div
+            className={getClassLink("popularity.desc")}
+            onClick={handleClick("popularity.desc")}
+          >
+            Popularity desc
+          </div>
+        </li>
+        <li className="nav-item">
+          <div
+            className={getClassLink("revenue.desc")}
+            onClick={handleClick("revenue.desc")}
+          >
+            Revenue desc
+          </div>
+        </li>
+        <li className="nav-item">
+          <div
+            className={getClassLink("vote_average.desc")}
+            onClick={handleClick("vote_average.desc")}
+          >
+            Vote avetage desc
+          </div>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default MovieTabs;
